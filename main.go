c79416db0ee51f70692aaaca323d66c6e81608b1
sdj <sdjthedeveloper@gmail.com>
2023-07-31 14:53:29 +0530

    wip

--- main.go
+++ main.go
@@ -5,11 +5,14 @@ import (
 	"net/http"
 
 	"github.com/GeekSpotlight/go-gin-example/config"
+	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.New()
+	// router.Use(gzip.Gzip(gzip.DefaultCompression))
+	router.Use(gzip.Gzip(gzip.BestCompression))
 
 	// serves http://localhost:8080/health
 	router.GET("/health", func(ctx *gin.Context) {
@@ -26,6 +29,9 @@ func main() {
 	router.GET("/bye", func(ctx *gin.Context) {
 		ctx.String(http.StatusOK, "take care now")
 	})
+
+	// http://localhost:8080/large-data-static
+	router.StaticFile("/large-data-static", "./resources/large_json_data.json")
 
 	// by default, runs in port :8080
 	router.Run()
